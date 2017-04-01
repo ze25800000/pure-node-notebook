1ab27ff7b70fcfdf20ea70802e96dafa85729411
ze258100000@sina.com <ze258100000@sina.comm>
2017-04-01 08:18:47 +0800

    lesson4 promise格式改写

--- App/index.js
+++ App/index.js
@@ -11,48 +11,24 @@ class App {
     return (req, res) => {
       //fs相对启动目录 process.cwd()
       let {url} = req;
-      // 所有以action结尾的url, 认为它是ajax
-      // let body = '';
-      // let headers = {};
-      /*if (url.match('action')) {
-       apiServer(url).then(val => {
-       body = JSON.stringify(val);
-       headers = {
-       'Content-Type': 'application/json'
-       };
-       let finalHeader = Object.assign(headers, {'X-powered-by': 'Node.js'});
-       res.writeHead(200, 'resolve ok', finalHeader);
-       res.end(body);
-       });
-       } else {
-       staticServer(url)
-       .then((data) => {
-       let finalHeader = Object.assign(headers, {'X-powered-by': 'Node.js'});
-       res.writeHead(200, 'resolve ok', finalHeader);
-       res.end(data);
-       })
-       .catch((err) => {
-       let finalHeader = Object.assign(headers, {'X-powered-by': 'Node.js'});
-       res.writeHead(200, 'resolve ok', finalHeader);
-       res.end(err);
-       });
-       }*/
-      res.setHeader('X-powered-by', 'Node.js');
-      apiServer(url)
-        .then(data => {
-          if (data == undefined) {
-            return staticServer(url); //如果apiServer中没找到对应请求, 则返回Promise{pending|fulfilled|rejected}
-          }
-          res.writeHead(200, 'resolve ok', {'Content-Type': 'application/json'});
-          res.end(JSON.stringify(data));
-        })
-        .then(data => { //当staticServer(url)的状态为Promise{fulfilled}时执行
-          res.end(data)
-        })
-        .catch(err => { //当staticServer(url)的状态为Promise{rejected}时执行
-          res.writeHead(404, 'not found');
-          res.end(err);
-        });
+      apiServer(url).then(val => {
+        if (!val) {
+          return staticServer(url)
+        } else {
+          return val;
+        }
+      }).then(val => {
+        let body = '';
+        let base = {'X-powered-by': 'Node.js'};
+        if (val instanceof Buffer) {
+          body = val;
+        } else {
+          body = JSON.stringify(val);
+          let finalHeader = Object.assign(base, {'Content-Type': 'application/json'});
+          res.writeHead(200, 'resolve ok', finalHeader);
+        }
+        res.end(body)
+      })
     };
   }
 }
--- test.js
+++ test.js
@@ -1,65 +1,17 @@
-function fn1(x) {
-  return new Promise((resolve, reject) => {
-    if (x == 1) {
-      resolve(x);
-    }
-    reject(x)
+function fn1() {
+  return Promise.resolve(123);
+}
+function fn2(url) {
+  return Promise.reject(url);
+}
+var p1 = fn1()
+  .then(res => {
+    console.log(res)
+    return fn2('hello')
   })
-}
-function fn2(x) {
-  var arr = [1, 2, 3, 4];
-  if (arr.indexOf(x) != -1) {
-    x = undefined;
-  }
-  return Promise.resolve(x);
-}
-var x = 3;
-fn2(x)
-  .then(val => fn1(x))
-  .catch(val => {
-    console.log('reject', val); //val 3
-  });
-/********************Promise.all([])*****************/
-var pa1 = new Promise((resolve, reject) => {
-  setTimeout(resolve, 1000, 'pa1');
-});
-var pa2 = new Promise((resolve, reject) => {
-  setTimeout(reject, 2000, 'pa2');
-});
-var pa3 = new Promise((resolve, reject) => {
-  setTimeout(reject, 1500, 'pa3');
-});
-var pa = Promise.all([pa1, pa2, pa3]);
-
-pa.then(val => {
-  console.log('resolve', val);
-})
-  .catch(val => {
-    console.log('reject', val);
-  });
-/********************Promise.race([])****************/
-var pr1 = new Promise((resolve, reject) => {
-  setTimeout(reject, 1000, 'pr1');
-});
-var pr2 = new Promise((resolve, reject) => {
-  setTimeout(reject, 1200, 'pr2');
-});
-var pr3 = new Promise((resolve, reject) => {
-  setTimeout(resolve, 800, 'pr3');
-});
-var pr = Promise.race([pr1, pr2, pr3]);
-pr.then(val => {
-  console.log('resolve', val);
-})
-  .catch(val => {
-    console.log('reject', val);
-  });
-/** *****************resolve|reject*******************/
-Promise.resolve(123).then(val => {
-  console.log(val);
-});
-Promise.reject(123).then(val => {
-  console.log(val);
-}).catch(val => {
-  console.log(val)
-});
+  .then(res => {
+    console.log('resolve', res)
+  })
+  .catch(res => {
+    console.log('reject', res)
+  })

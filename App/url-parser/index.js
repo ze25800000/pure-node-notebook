06776fa942b91b33ef6f54bc34a476cecf6f1fbe
ze258100000@sina.com <ze258100000@sina.comm>
2017-04-05 17:23:59 +0800

    将url-parser的post改为buffer.concat

--- App/url-parser/index.js
+++ App/url-parser/index.js
@@ -6,11 +6,11 @@ module.exports = (ctx) => {
   return Promise.resolve({
     then: (resolve, reject) => {
       if (method == 'post') {
-        let data = "";
+        let data = [];
         ctx.req.on('data', (chunk) => {
-          data += chunk;
+          data.push(chunk);
         }).on('end', () => {
-          reqCtx.body = JSON.parse(data);
+          reqCtx.body = JSON.parse(Buffer.concat(data).toString());
           resolve();
         });
       } else {
